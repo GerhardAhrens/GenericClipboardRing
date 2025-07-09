2c614f0de907269ebec06b0b114be8141c29491e
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-07-09 20:19:21 +0200

    Projektdateien hinzufügen.

---
+++ ClipboardRing/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.cs" company="Lifeprojects.de">
+//     Class: MainWindow
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>09.07.2025 19:41:25</date>
+//
+// <summary>
+// MainWindow mit Minimalfunktionen
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace ClipboardRing
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Ribbon;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+            WeakEventManager<Window, CancelEventArgs>.AddHandler(this, "Closing", this.OnWindowClosing);
+
+            this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Copy, (sender, args) => { this.CopyContent(this,null); }));
+            this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, (sender, args) => { this.PasteContent(this, null); }));
+
+            this.WindowTitel = "Generic Clipboard Ring";
+            this.DataContext = this;
+        }
+
+        private string _WindowTitel;
+
+        public string WindowTitel
+        {
+            get { return _WindowTitel; }
+            set
+            {
+                if (this._WindowTitel != value)
+                {
+                    this._WindowTitel = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _TextUndZahlen;
+
+        public string TextUndZahlen
+        {
+            get { return _TextUndZahlen; }
+            set
+            {
+                if (this._TextUndZahlen != value)
+                {
+                    this._TextUndZahlen = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _BoolWert;
+
+        public bool BoolWert
+        {
+            get { return _BoolWert; }
+            set
+            {
+                if (this._BoolWert != value)
+                {
+                    this._BoolWert = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WeakEventManager<Button, RoutedEventArgs>.AddHandler(this.BtnCloseApplication, "Click", this.OnCloseApplication);
+
+        }
+
+        private void OnCloseApplication(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            MessageBoxResult msgYN = MessageBox.Show("Wollen Sie die Anwendung beenden?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (msgYN == MessageBoxResult.Yes)
+            {
+                App.ApplicationExit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void CopyContent(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PasteContent(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region INotifyPropertyChanged implementierung
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler == null)
+            {
+                return;
+            }
+
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+        #endregion INotifyPropertyChanged implementierung
+    }
+}
